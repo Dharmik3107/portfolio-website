61a9ac77965ce2c53dbb39d49f1afd18154d0946
Dharmik Abhangi <abhangidharmik@gmail.com>
2023-01-31 03:43:11 +0530

    feat:UI Issue Solved - I

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -3,7 +3,7 @@ import Button from "../Button/Button";
 
 const Blog = () => {
   return (
-    <div className="w-full phone:h-fit tablet:h-[100px] laptop:h-[100px] mb-5 flex phone:flex-col px-2 tablet:flex-row justify-center items-center border-2 border-solid border-[#00FFc2]">
+    <div className="w-full h-fit mb-5 flex phone:flex-col px-2 tablet:flex-row justify-center items-center border-2 border-solid border-[#00FFc2]">
       <div className="flex flex-col justify-center phone:items-center phone:text-justify tablet:text-left tablet:items-start inter text-white p-2">
         <h5 className="font-extrabold text-lg">Title</h5>
         <p className="font-normal text-xs ">
--- src/Components/BlogsText/BlogsText.jsx
+++ src/Components/BlogsText/BlogsText.jsx
@@ -3,7 +3,7 @@ import Blog from "../Blog/Blog";
 
 const BlogsText = () => {
   return (
-    <div className="w-full max-w-[720px] h-[450px] flex flex-col justify-start items-center overflow-hidden">
+    <div className="w-full max-w-[720px] h-[450px] flex flex-col justify-start items-center">
       <h1 className="text-center phone:text-3xl tablet:text-4xl laptop:text-6xl openSans text-[#00FFC2] font-extrabold leading-[68px] mb-1">
         Blogs
       </h1>
