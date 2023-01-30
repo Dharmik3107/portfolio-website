846d88ed20472c839932ad1058926efc37ff5cd0
Dharmik Abhangi <abhangidharmik@gmail.com>
2023-01-30 12:52:16 +0530

    feat:Sentry setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 
+Sentry.init({
+  dsn: "https://9395e2bfaa644765b7e8195b2ae5368e@o4504149477687296.ingest.sentry.io/4504592263282688",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
